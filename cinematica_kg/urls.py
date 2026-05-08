61ae5a5c297c2c2672573ebf81789bece88f6074
tanaki <tanaki.93@gmail.com>
2026-05-08 16:56:05 +0600

    lesson 4

--- cinematica_kg/urls.py
+++ cinematica_kg/urls.py
@@ -20,6 +20,6 @@ from films import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/films/', views.film_list_api_view),
-    path('api/v1/films/<int:id>/', views.film_detail_api_view),
+    path('api/v1/films/', views.film_list_create_api_view),  # GET->list, POST->create
+    path('api/v1/films/<int:id>/', views.film_detail_api_view),  # GET->item, PUT->update, DELETE->destroy
 ]
--- films/views.py
+++ films/views.py
@@ -5,27 +5,72 @@ from .serializers import FilmListSerializer, FilmDetailSerializer
 from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def film_detail_api_view(request, id):
     try:
         film = Film.objects.get(id=id)
     except:
         return Response(data={'message': 'film not found'},
                         status=status.HTTP_404_NOT_FOUND)
-    data = FilmDetailSerializer(film, many=False).data
-    return Response(data=data)
+    if request.method == 'GET':
+        data = FilmDetailSerializer(film, many=False).data
+        return Response(data=data)
+    elif request.method == 'DELETE':
+        film.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        film.title = request.data.get('title')
+        film.text = request.data.get('text')
+        film.release_year = request.data.get('release_year')
+        film.rating = request.data.get('rating')
+        film.is_hit = request.data.get('is_hit')
+        film.director_id = request.data.get('director_id')
+        film.genres.set(request.data.get('genres'))
+        film.save()
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data=FilmDetailSerializer(film).data
+        )
 
 
-@api_view(['GET'])
-def film_list_api_view(request):
-    # step 1: collect films (QuerySet)
-    films = (Film.objects.select_related('director')
-             .prefetch_related('genres', 'reviews').all())
+@api_view(['GET', 'POST'])
+def film_list_create_api_view(request):
+    if request.method == 'GET':
+        # step 1: collect films (QuerySet)
+        films = (Film.objects.select_related('director')
+                 .prefetch_related('genres', 'reviews').all())
 
-    # step 2: reformat films to list of dictionary
-    data = FilmListSerializer(films, many=True).data
+        # step 2: reformat films to list of dictionary
+        data = FilmListSerializer(films, many=True).data
 
-    # step 3: return response
-    return Response(
-        data=data,  # list, dict
-    )
+        # step 3: return response
+        return Response(
+            data=data,  # list, dict
+        )
+    elif request.method == 'POST':
+        # step 1: receive data
+        title = request.data.get('title')
+        text = request.data.get('text')
+        release_year = request.data.get('release_year')
+        rating = request.data.get('rating')
+        is_hit = request.data.get('is_hit')
+        director_id = request.data.get('director_id')
+        genres = request.data.get('genres')
+
+        # step 2: create film
+        film = Film.objects.create(
+            title=title,
+            text=text,
+            release_year=release_year,
+            rating=rating,
+            is_hit=is_hit,
+            director_id=director_id,
+        )
+        film.genres.set(genres)
+        film.save()
+
+        # step 3: return response
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data=FilmDetailSerializer(film).data
+        )
